0ea1f4e3e0eb628b1b13dfa25c55c2994d24df94
s24965-pj <92219386+Dawid0132@users.noreply.github.com>
2025-12-30 19:45:37 +0100

    GithubConnectorService init

---
+++ code-audit-engine-backend/src/main/java/com/example/githubconnectorservice/GithubConnectorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubconnectorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubConnectorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubConnectorServiceApplication.class, args);
+    }
+
+}
---
+++ code-audit-engine-backend/src/test/java/com/example/githubconnectorservice/GithubConnectorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubconnectorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubConnectorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
