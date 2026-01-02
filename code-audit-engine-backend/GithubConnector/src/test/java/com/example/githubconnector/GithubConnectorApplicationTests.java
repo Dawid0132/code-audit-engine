aba30b6db3fae6eea9a4396b317896a46adc8a06
DawidJanas123 <ksenog1@gmail.com>
2026-01-02 18:06:10 +0100

    add rest files of GithubConnectorApplication

---
+++ code-audit-engine-backend/GithubConnector/src/test/java/com/example/githubconnector/GithubConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubConnectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
