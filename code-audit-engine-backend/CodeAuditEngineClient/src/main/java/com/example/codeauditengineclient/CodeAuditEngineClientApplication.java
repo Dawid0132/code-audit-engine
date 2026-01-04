7224880ac88b2333f4faa6fda5c8001ffae529f5
DawidJanas123 <ksenog1@gmail.com>
2026-01-04 11:04:08 +0100

    add Gateway and Client module based on spring boot

---
+++ code-audit-engine-backend/CodeAuditEngineClient/src/main/java/com/example/codeauditengineclient/CodeAuditEngineClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeauditengineclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeAuditEngineClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeAuditEngineClientApplication.class, args);
+    }
+
+}
---
+++ code-audit-engine-backend/CodeAuditEngineClient/src/test/java/com/example/codeauditengineclient/CodeAuditEngineClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeauditengineclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeAuditEngineClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
