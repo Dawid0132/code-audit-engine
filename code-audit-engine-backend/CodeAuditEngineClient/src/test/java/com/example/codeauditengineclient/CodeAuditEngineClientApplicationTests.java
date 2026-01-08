44003707d50868f513d5399fb5299e8ca46ec554
DawidJanas123 <ksenog1@gmail.com>
2026-01-08 12:27:35 +0100

    add clientRegistrationRepository as Mock in test

--- code-audit-engine-backend/CodeAuditEngineClient/src/test/java/com/example/codeauditengineclient/CodeAuditEngineClientApplicationTests.java
+++ code-audit-engine-backend/CodeAuditEngineClient/src/test/java/com/example/codeauditengineclient/CodeAuditEngineClientApplicationTests.java
@@ -2,13 +2,18 @@ package com.example.codeauditengineclient;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @Import(TestSecurityConfig.class)
 @ActiveProfiles("test")
 class CodeAuditEngineClientApplicationTests {
+
+    @MockBean
+    ClientRegistrationRepository clientRegistrationRepository;
 
     @Test
     void contextLoads() {
