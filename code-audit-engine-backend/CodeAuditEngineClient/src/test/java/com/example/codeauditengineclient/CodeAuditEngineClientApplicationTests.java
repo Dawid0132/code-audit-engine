2d9c44b3c8cc20db6076789beea48215adedf744
DawidJanas123 <ksenog1@gmail.com>
2026-01-08 13:00:42 +0100

    disable Keycloak for tests

--- code-audit-engine-backend/CodeAuditEngineClient/src/test/java/com/example/codeauditengineclient/CodeAuditEngineClientApplicationTests.java
+++ code-audit-engine-backend/CodeAuditEngineClient/src/test/java/com/example/codeauditengineclient/CodeAuditEngineClientApplicationTests.java
@@ -2,14 +2,10 @@ package com.example.codeauditengineclient;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-@Import(TestSecurityConfig.class)
 class CodeAuditEngineClientApplicationTests {
 
     @Test
--- code-audit-engine-backend/Gateway/src/test/java/com/example/gateway/GatewaySecurityTest.java
+++ code-audit-engine-backend/Gateway/src/test/java/com/example/gateway/GatewaySecurityTest.java
@@ -2,12 +2,10 @@ package com.example.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-@Import(TestSecurityConfig.class)
 class GatewaySecurityTest {
 
     @Test
